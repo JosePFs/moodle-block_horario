72e76080aa37789ca5e54f7851a529c217106db7
José Puente <jpuentefs@gmail.com>
2016-10-16 21:31:14 +0200

    Fix required version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016100701;
-$plugin->requires  = 2010112400;
+$plugin->requires  = 2011120500;
 $plugin->cron      = 0;
 $plugin->component = 'block_horario';
 $plugin->maturity  = MATURITY_RC;
