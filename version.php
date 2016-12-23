0fb08e850236d31b3cc910b262ef07c7a6c092ab
José Puente <jpuentefs@gmail.com>
2016-12-23 21:14:12 +0100

    Added required version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016100702;
-$plugin->requires  = 2013111811;
+$plugin->requires  = 2011120511;
 $plugin->cron      = 0;
 $plugin->component = 'block_horario';
 $plugin->maturity  = MATURITY_RC;
